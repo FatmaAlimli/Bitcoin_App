02b89ab6dc7e70edf94351f1185080fd0265a6a7
fatma <fatma.alimli35@gmail.com>
2020-07-28 22:24:17 +0300

    CoinNewsFragment eksikleri giderildi.

--- app/src/main/java/com/example/bitcoinuygulamas/CoinNewsFragment.java
+++ app/src/main/java/com/example/bitcoinuygulamas/CoinNewsFragment.java
@@ -1,18 +1,27 @@
 package com.example.bitcoinuygulamas;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class CoinNewsFragment extends Fragment {
 
     View view;
     TextView titleTextView;
+    private String url = "https://www.coindesk.com";
+    private WebView webView;
+    private ProgressBar progressBar;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,8 +34,53 @@ public class CoinNewsFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.view = view;
-        titleTextView = (TextView) view.findViewById(R.id.textTitle);
 
+
+        webView = view.findViewById(R.id.web_view);
+        progressBar = view.findViewById(R.id.progress_circular);
+        swipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+
+        webView.loadUrl(url);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                webView.reload();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                webView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                webView.setVisibility(View.VISIBLE);
+
+
+                if (swipeRefreshLayout.isRefreshing()) {
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+
+                if (progressBar.getVisibility() == View.VISIBLE) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+
+        });
 
     }
+
+
 }
